9e50a77264e7655088be1ea9efa4f9edc99b71be
Marcus Koh <kohchihao@gmail.com>
2020-10-05 21:56:01 +0800

    Add _app,  _document and chakra-ui

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
+import theme from '@styles/theme';
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <ColorModeProvider value="light">
+        <CSSReset />
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
