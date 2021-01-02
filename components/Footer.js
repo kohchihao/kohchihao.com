4210ca1bd0e37a438b09fa27fdbb23164e23bbc5
Marcus Koh <kohchihao@gmail.com>
2021-01-02 22:07:59 +0800

    Update footer icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import { Flex, Text, Stack, Link, IconButton } from '@chakra-ui/react';
+import { Flex, Link, IconButton } from '@chakra-ui/react';
+import { FaGithub, FaLinkedin, FaMedium } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
 const Footer = () => {
   return (
     <Flex flexDirection="column" justifyContent="center" alignItems="center" w="100%">
       <Flex>
         <Link href="https://github.com/kohchihao" title="GitHub" isExternal>
-          <IconButton aria-label="GitHub" icon="github" size="lg" color="gray.500" variant="ghost" />
+          <IconButton aria-label="GitHub" icon={<FaGithub />} size="lg" color="gray.500" variant="ghost" />
         </Link>
 
         <Link href="https://www.linkedin.com/in/kohchihao/" title="LinkedIn" isExternal>
-          <IconButton aria-label="LinkedIn" icon="linkedin" size="lg" color="gray.500" variant="ghost" />
+          <IconButton aria-label="LinkedIn" icon={<FaLinkedin />} size="lg" color="gray.500" variant="ghost" />
         </Link>
 
         <Link href="mailto:kohchihao@gmail.com" title="Mail" isExternal>
-          <IconButton aria-label="Mail" icon="mail" size="lg" color="gray.500" variant="ghost" />
+          <IconButton aria-label="Mail" icon={<MdEmail />} size="lg" color="gray.500" variant="ghost" />
         </Link>
 
         <Link href="https://medium.com/@kohchihao" title="Medium" isExternal>
-          <IconButton aria-label="Medium" icon="medium" size="lg" color="gray.500" variant="ghost" />
+          <IconButton aria-label="Medium" icon={<FaMedium />} size="lg" color="gray.500" variant="ghost" />
         </Link>
       </Flex>
     </Flex>
